aa01ea5d6a439186dd4ba2b7627426ef2a622ca9
nurbolot2006 <kemelbekovnurbolot201@gmail.com>
2025-09-26 14:19:24 +0600

    Kaitech-3

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import {ProjectsSection} from "./projectsSection/ProjectsSection.tsx";
+import {Services} from "./services/Services.tsx";
 
 const Home: React.FC = () => {
     return (
         <div>
             <ProjectsSection/>
+            <Services/>
         </div>
     );
 };
