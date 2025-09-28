bb6d875ef18d88169369073c92f1c60be9882c90
nurbolot2006 <kemelbekovnurbolot201@gmail.com>
2025-09-28 14:42:28 +0600

    Kaitech-5

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Banner from "../widgets/banner/Banner";
 import { ProjectsSection } from "./projectsSection/ProjectsSection.tsx";
 import { Services } from "./services/services/Services.tsx";
+import {Team} from "./team/team/Team.tsx";
 
 const Home: React.FC = () => {
     return (
@@ -9,6 +10,7 @@ const Home: React.FC = () => {
             <Banner />
             <ProjectsSection />
             <Services />
+            <Team/>
         </div>
     );
 };
